a490ae5bc2310c5eaa7fa6f240b3b6dfc64b0ee3
Santi:ETH <ethx42@gmail.com>
2023-04-09 21:13:57 -0500

    update transcribeHAndler controller, remove staled code

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,22 +2,17 @@ const fs = require('fs');
 const { OpenAIApi } = require('openai');
 const configuration = require('../config');
 
-const openai = new OpenAIApi(configuration);
+const openAi = new OpenAIApi(configuration);
 
 const transcribeHandler = async (req, res) => {
-  try {
-    await openai.createTranscription(
-      fs.createReadStream(req.file.path),
-      'whisper-1'
-    ).then(resOpenAI => {
-      res.status(resOpenAI.status).json({ message: resOpenAI.data.text, filePath: req.file.path });
-    }).catch(err => {
-      res.status(err.response.status).json({ error: `OpenAI: ${err.response.data.error.message}` });
-    });
-  } catch (error) {
-    console.error('Error Calling Whisper ASR API:', error);
-    res.status(500).json({ error: error.code });
-  }
+  await openAi.createTranscription(
+    fs.createReadStream(req.file.path),
+    'whisper-1'
+  ).then(resOpenAI => {
+    res.status(resOpenAI.status).json({ message: resOpenAI.data.text });
+  }).catch(e => {
+    res.status(e.response.status).json({ error: e.response.data.error.message });
+  });
 };
 
 module.exports = {
